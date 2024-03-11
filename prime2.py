40ceeac4efd876857c43d7ecef3727ca786ac9c6
Aniketgoyal12 <143060619+Aniketgoyal12@users.noreply.github.com>
2024-03-11 21:05:43 +0530

    Update prime2.py

--- prime2.py
+++ prime2.py
@@ -9,3 +9,14 @@ if count == 1:
 else:
     print("NUmber is not prime")
         
+APPROACH 2:
+
+n = int (input("Enter the number"))
+count = 0
+for i in range(1,n+1,1):
+    if n%i != 0:
+        count =+ 1
+if count == 2:
+    print("Number is  prime")
+else:
+    print("Number is not prime")
