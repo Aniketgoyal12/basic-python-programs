d4f574866caecc042a93ec2e1b45081535bb19d4
Aniketgoyal12 <143060619+Aniketgoyal12@users.noreply.github.com>
2024-03-15 19:53:54 +0530

    Update removelistduplicate.py

--- removelistduplicate.py
+++ removelistduplicate.py
@@ -6,11 +6,10 @@
 
 l = ['apple','banana','apple','strawberry','mango','blackberry']
 a = input("Enter the element you want to remove duplicates")
-#count = 0
+count = 0
 for i in range(0,len(l)):
     if l[i] == a:
         count += 1
-print(count)
 if count >1:
     l.remove(a)
 print(l)
